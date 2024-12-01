fadf63e7d66e09a836f388d15b35926a3db5fe5e
Reutm1502 <reutm150208@mevoot.co.il>
2024-12-01 17:48:05 +0200

    yes yes

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Reut & Noa are your Queens!!!!!')
+    print_hi('Reut & Noa are your Queens!!!!! hotamar lozer')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
